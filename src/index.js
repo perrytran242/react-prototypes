61ddadc3116cabc2e609b5a704a8da4b47c333dd
Perry Tran <ptran209@gmail.com>
2018-09-12 21:35:35 -0700

    Prototype 1 React Intro

--- src/index.js
+++ src/index.js
@@ -1,7 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const luckyNumber = () => {
+    return Math.floor( (Math.random() * 1000) + 1);
+}
+
+const user = {
+    name: 'Perry',
+    luckyNumber: luckyNumber(),
+}
+
+const greeting = (user) => {
+   const element = (
+       <div className="container">
+            <h1>Hello {user.name}!</h1>
+            <h2 className="text-muted">Your lucky number is {user.luckyNumber}</h2>
+       </div>
+   )
+   return element;  
+}
+
+
+
 ReactDOM.render(
-    <h1 className="container">React Prototypes!</h1>,
+    // <h1 className="container">React Prototypes!</h1>,
+    greeting(user),
     document.getElementById('root')
 );
