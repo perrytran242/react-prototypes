6b0250bd753d8b65b5e0e080f4625bd21fa57078
Perry Tran <ptran209@gmail.com>
2018-09-15 21:46:04 -0700

    Component State prototype - Perry Tran

--- src/components/app.js
+++ src/components/app.js
@@ -1,2 +1,10 @@
+import React from 'react';
 
+const App = () => {
+    return (
+        <div className="container">
+            <h1 className="text-center">Stop Watch</h1>
+        </div>
+    )
+}
 
